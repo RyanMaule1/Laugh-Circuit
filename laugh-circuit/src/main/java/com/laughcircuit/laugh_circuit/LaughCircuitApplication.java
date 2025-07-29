b28fc06f55ddd3d04b5a234aa1154a1d9fc15a7d
Ryan Maule <ryan_maule@yahoo.com>
2025-07-29 09:06:24 -0500

    basic spring boot setup

---
+++ laugh-circuit/src/main/java/com/laughcircuit/laugh_circuit/LaughCircuitApplication.java
@@ -0,0 +1,14 @@
+package com.laughcircuit.laugh_circuit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaughCircuitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaughCircuitApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
