96609c130a54633d961d01f542ae70c5570b1354
Ryan Maule <ryan_maule@yahoo.com>
2025-07-30 16:02:34 -0500

    docker files implemented but not working and switched to jdk 21

--- server/src/main/java/com/laughcircuit/laugh_circuit/Controller.java
+++ server/src/main/java/com/laughcircuit/laugh_circuit/Controller.java
@@ -1,12 +1,7 @@
 package com.laughcircuit.laugh_circuit;
 
-import java.io.File;
 import java.util.concurrent.atomic.AtomicLong;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
